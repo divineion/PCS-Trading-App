5f90cefa4dd6bbfa1fb948db490cd142b2fdca53
divineion <najima.guermoudi@gmail.com>
2025-06-20 13:11:55 +0200

    fix(database): move data initialization script into classpath
     - move `data.sql` from `doc/` to `src/main/resources/db/` to ensure it
    is loaded from the classpath.
     - split initialization into two files : `schema.sql` for schema
    definitions and `data.sql` for initial data.
     - copy both files into the test resources directory for test
    environment.
     - rename tables to follow snake_case convention.

---
+++ Poseiden-skeleton/src/main/resources/db/schema.sql
@@ -0,0 +1,83 @@
+CREATE TABLE IF NOT EXISTS BidList (
+  id tinyint(4) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  account VARCHAR(30) NOT NULL,
+  type VARCHAR(30) NOT NULL,
+  bid_quantity DOUBLE,
+  ask_quantity DOUBLE,
+  bid DOUBLE ,
+  ask DOUBLE,
+  benchmark VARCHAR(125),
+  bid_list_date TIMESTAMP,
+  commentary VARCHAR(125),
+  security VARCHAR(125),
+  status VARCHAR(10),
+  trader VARCHAR(125),
+  book VARCHAR(125),
+  creation_name VARCHAR(125),
+  creation_date TIMESTAMP ,
+  revision_name VARCHAR(125),
+  revision_date TIMESTAMP ,
+  deal_name VARCHAR(125),
+  deal_type VARCHAR(125),
+  source_list_id VARCHAR(125),
+  side VARCHAR(125)
+);
+
+CREATE TABLE IF NOT EXISTS Trade (
+  id tinyint(4) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  account VARCHAR(30) NOT NULL,
+  type VARCHAR(30) NOT NULL,
+  buy_quantity DOUBLE,
+  sell_quantity DOUBLE,
+  buy_price DOUBLE ,
+  sell_price DOUBLE,
+  trade_date TIMESTAMP,
+  security VARCHAR(125),
+  status VARCHAR(10),
+  trader VARCHAR(125),
+  benchmark VARCHAR(125),
+  book VARCHAR(125),
+  creation_name VARCHAR(125),
+  creation_date TIMESTAMP ,
+  revision_name VARCHAR(125),
+  revision_date TIMESTAMP ,
+  deal_name VARCHAR(125),
+  deal_type VARCHAR(125),
+  source_list_id VARCHAR(125),
+  side VARCHAR(125)
+);
+
+CREATE TABLE IF NOT EXISTS CurvePoint (
+  id tinyint(4) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  curve_id tinyint(4) NOT NULL,
+  as_of_date TIMESTAMP,
+  term DOUBLE ,
+  value DOUBLE ,
+  creation_date TIMESTAMP
+  );
+
+CREATE TABLE IF NOT EXISTS Rating (
+  id tinyint(4) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  moodys_rating VARCHAR(125),
+  sand_p_rating VARCHAR(125),
+  fitch_rating VARCHAR(125),
+  order_number tinyint
+);
+
+CREATE TABLE IF NOT EXISTS RuleName (
+  Id tinyint(4) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(125),
+  description VARCHAR(125),
+  json VARCHAR(125),
+  template VARCHAR(512),
+  sql_str VARCHAR(125),
+  sql_part VARCHAR(125)
+);
+
+CREATE TABLE IF NOT EXISTS Users (
+  Id tinyint(4) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  username VARCHAR(125),
+  password VARCHAR(125),
+  fullname VARCHAR(125),
+  role VARCHAR(125)
+);
