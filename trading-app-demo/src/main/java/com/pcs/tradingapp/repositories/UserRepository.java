7207616704cdb5d2425fdc6194f3443db620e231
divineion <najima.guermoudi@gmail.com>
2025-06-24 19:24:46 +0200

    feat(DAL): add method `findByUsername()` in UserRepository
    Accepts String username.
    Used to check if a username already exists in the database.

--- trading-app-demo/src/main/java/com/pcs/tradingapp/repositories/UserRepository.java
+++ trading-app-demo/src/main/java/com/pcs/tradingapp/repositories/UserRepository.java
@@ -1,5 +1,7 @@
 package com.pcs.tradingapp.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
@@ -7,5 +9,5 @@ import com.pcs.tradingapp.domain.User;
 
 
 public interface UserRepository extends JpaRepository<User, Integer>, JpaSpecificationExecutor<User> {
-
+	Optional<User> findByUsername(String username);
 }
