1788c7e3c43acb935dc40f02ab57d23d687cf1ea
divineion <najima.guermoudi@gmail.com>
2025-06-24 02:58:06 +0200

    feat(role): add role in schema and data initialization
    In schema.sql, add the role table with a primary key on `id` and a
    unique index on `name`.
    Modify the `user` table to include a foreign key on the `role` field,
    referencing the `id` field of the `role` table.
    
    In data.sql, modify users insertions and insert initial roles using
    INSERT INTO... SELECT... WHERE NOT EXISTS to prevent duplicate errors.

--- trading-app-demo/src/main/resources/db/data.sql
+++ trading-app-demo/src/main/resources/db/data.sql
@@ -1,2 +1,39 @@
-insert into Users(fullname, username, password, role) values("Administrator", "admin", "$2a$10$pBV8ILO/s/nao4wVnGLrh.sa/rnr5pDpbeC4E.KNzQWoy8obFZdaa", "ADMIN");
-insert into Users(fullname, username, password, role) values("User", "user", "$2a$10$pBV8ILO/s/nao4wVnGLrh.sa/rnr5pDpbeC4E.KNzQWoy8obFZdaa", "USER");
+-- Data for table role
+INSERT INTO `role` (id, name) 
+SELECT * FROM (SELECT 1, 'USER') as tmp
+WHERE NOT EXISTS (SELECT 1 FROM role WHERE name = 'USER')
+LIMIT 1;
+
+
+INSERT INTO `role` (id, name) 
+SELECT * FROM (SELECT 2, 'ADMIN') as tmp
+WHERE NOT EXISTS (SELECT 1 FROM role WHERE name = 'ADMIN')
+LIMIT 1;
+
+-- Data for table app_user
+
+INSERT INTO app_user (fullname, username, password, role)
+SELECT * FROM (
+	SELECT 
+		"Administrator" as fullname, 
+		"admin" as username, 
+		"$2a$10$pBV8ILO/s/nao4wVnGLrh.sa/rnr5pDpbeC4E.KNzQWoy8obFZdaa" as password, 
+		2 as role
+	) as tmp
+WHERE NOT EXISTS (
+	SELECT 1 FROM app_user WHERE username = "admin"
+)
+LIMIT 1;
+
+INSERT INTO app_user (fullname, username, password, role) 
+SELECT * FROM (
+	SELECT 
+		"User" as fullname, 
+		"user" as username, 
+		"$2a$10$pBV8ILO/s/nao4wVnGLrh.sa/rnr5pDpbeC4E.KNzQWoy8obFZdaa" as password, 
+		1 as role
+	) AS tmp
+WHERE NOT EXISTS (
+	SELECT 1 FROM app_user WHERE username = "user"
+)
+LIMIT 1;
