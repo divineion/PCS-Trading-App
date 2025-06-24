bb4f634e1a1c1b0d99a72bce8e2718cebf65cce3
divineion <najima.guermoudi@gmail.com>
2025-06-24 19:23:03 +0200

    fix(validation): correct constraint annotations
    Correct annotations for properties:
     - username: add `@Size` and error message;
     - fullname: correct regex to accept compound name with "-" or space;
     - password: remove duplicate `@Size` (size is already defined in regex)

--- trading-app-demo/src/main/java/com/pcs/tradingapp/dto/request/CreateUserDto.java
+++ trading-app-demo/src/main/java/com/pcs/tradingapp/dto/request/CreateUserDto.java
@@ -5,18 +5,23 @@ import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public class CreateUserDto {
-	@NotBlank(message = "Username is mandatory") @Size(min=3, max=30) 
+	@NotBlank(message = "Username is mandatory.") 
+	@Size(min=3, max=30, message="The username must contain between 3 and 30 characters.") 
 	private String username;
 	
-	@Size(min = 8, max = 30, message = "The password must contain between 8 and 30 characters")
+	@NotBlank(message = "Password is mandatory.")
 	@Pattern(
 	        regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&_]).{8,30}$",
-	        message = "The password must contain at least one upper, one lower, one number and one special character"
+	        message = "The password must contain at least one upper, one lower, one number and one special character."
 	    )
-	@NotBlank(message = "Password is mandatory")
 	private String password;
 	
-	@NotBlank(message = "FullName is mandatory") 
+	@NotBlank(message = "Fullname is mandatory")
+	@Size(min=2, max=30, message = "The fullname must contain between 2 and 30 characters.")
+	@Pattern(
+			regexp = "^([A-Za-zÀ-ÖØ-öø-ÿ]{2,})([- ][A-Za-zÀ-ÖØ-öø-ÿ]{2,})*$",
+			message = "Each part of the fullname must contain at least 2 letters."
+			)
 	private String fullname;
 	
 	@NotBlank(message = "Role is mandatory") 
