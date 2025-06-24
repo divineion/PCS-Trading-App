2f48ef41f9446e6d7e7af6fd094e2ba081e6dfc2
divineion <najima.guermoudi@gmail.com>
2025-06-24 05:07:04 +0200

    feat(exceptions): add custom exceptions for user management
    - Add RoleNotFoundException to handle cases where a role is not found
    (should never happen as roles are managed by the database).
    
    - Add UsernameAlreadyExistsException to handle attempts to create a user
    with an existing username.
    
    - Add UserNotFoundException to handle cases where a user is not found.

---
+++ trading-app-demo/src/main/java/com/pcs/tradingapp/exceptions/RoleNotFoundException.java
@@ -0,0 +1,13 @@
+package com.pcs.tradingapp.exceptions;
+
+public class RoleNotFoundException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public RoleNotFoundException(String message) {
+		super(message);
+	}
+
+}
