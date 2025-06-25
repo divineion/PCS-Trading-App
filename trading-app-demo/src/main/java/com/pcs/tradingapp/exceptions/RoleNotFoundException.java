80e4990978a0b8fde9eb24a3e3d8c8cb5e758d97
divineion <najima.guermoudi@gmail.com>
2025-06-25 21:51:39 +0200

    feat(error): add HTTP 500 response for RoleNotFoundException
    Annotate exception with `@ResponseStatus` to return HTTP 500 INTERNAL
    SERVER ERROR status for RoleNotFoundException (this case should never
    occur in normal conditions, as the available roles are defined and
    persisted in the DB)

--- trading-app-demo/src/main/java/com/pcs/tradingapp/exceptions/RoleNotFoundException.java
+++ trading-app-demo/src/main/java/com/pcs/tradingapp/exceptions/RoleNotFoundException.java
@@ -1,5 +1,9 @@
 package com.pcs.tradingapp.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
 public class RoleNotFoundException extends Exception {
 	/**
 	 * 
