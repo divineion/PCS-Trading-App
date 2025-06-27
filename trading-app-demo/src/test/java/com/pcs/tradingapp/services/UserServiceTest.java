b039ae8988f0ca6d10d2d72a2dcdecc4f751ca1d
divineion <najima.guermoudi@gmail.com>
2025-06-27 02:18:20 +0200

    feat(tests): add unit test for UserService
    Add tests to ensure that exception are thrown if:
     - the requested role does not exist in the repository
     - the requested user does not exist in the repository

---
+++ trading-app-demo/src/test/java/com/pcs/tradingapp/services/UserServiceTest.java
@@ -0,0 +1,47 @@
+package com.pcs.tradingapp.services;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.pcs.tradingapp.domain.RoleName;
+import com.pcs.tradingapp.domain.User;
+import com.pcs.tradingapp.exceptions.RoleNotFoundException;
+import com.pcs.tradingapp.exceptions.UserNotFoundException;
+import com.pcs.tradingapp.repositories.RoleRepository;
+import com.pcs.tradingapp.repositories.UserRepository;
+
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTest {
+	@Mock
+	private RoleRepository roleRepository;
+	
+	@Mock
+	private UserRepository userRepository;
+	
+	@InjectMocks
+	private UserService service;
+	
+	@Test
+	public void testFetchRole_shouldThrowException() throws Exception {
+		//assert: repository don't find the role
+		when(roleRepository.findByName(RoleName.USER)).thenReturn(null);
+		
+		//act and assert: service shoul throw exception
+		assertThrows(RoleNotFoundException.class, ()-> service.fetchUserRole("USER", new User()));
+	}
+	
+	@Test
+	public void testValidateUserExists_shouldThrowException() throws Exception {
+		int userId = 999;
+		when(userRepository.findById(userId)).thenReturn(null);
+		
+		assertThrows(UserNotFoundException.class, ()-> service.validateUserExists(userId));
+	}
+}
