886db5ee91706c69c781bf8dfc0c5a8331802029
divineion <najima.guermoudi@gmail.com>
2025-06-30 15:07:02 +0200

    feat(bidlist): implement BidListService with CRUD logic
     - add create, update, fetch and delete methods
     - use BidListMapper to convert between entities and DTOs
     - handle BidListNotFoundException

---
+++ trading-app-demo/src/main/java/com/pcs/tradingapp/services/BidListService.java
@@ -0,0 +1,52 @@
+package com.pcs.tradingapp.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.pcs.tradingapp.constants.ApiMessages;
+import com.pcs.tradingapp.domain.BidList;
+import com.pcs.tradingapp.dto.request.bidlist.CreateBidListDto;
+import com.pcs.tradingapp.dto.request.bidlist.UpdateBidListDto;
+import com.pcs.tradingapp.dto.response.BidListInfoDto;
+import com.pcs.tradingapp.exceptions.BidListNotFoundException;
+import com.pcs.tradingapp.repositories.BidListRepository;
+
+@Service
+public class BidListService {
+	
+	private final BidListRepository repository;
+	private final BidListMapper mapper;
+	
+	public BidListService(BidListRepository repository, BidListMapper mapper) {
+		this.repository = repository;
+		this.mapper = mapper;
+	}
+
+	public List<BidListInfoDto> getAllBidLists() {
+		List<BidList> bidLists = repository.findAll();
+		return mapper.bidListsToBidListInfoDtos(bidLists);
+	}
+
+	public void createNewBidList(CreateBidListDto bidListDto) {
+		BidList bidList = mapper.createBidListDtoToBidList(bidListDto);
+		repository.save(bidList);
+	}
+
+	public UpdateBidListDto fetchUpdateBidListDtoById(Integer id) throws BidListNotFoundException {
+		BidList bidList = repository.findById(id)
+				.orElseThrow(() -> new BidListNotFoundException(ApiMessages.BIDLIST_NOT_FOUND));
+		
+		return mapper.bidListToUpdateBidListDto(bidList);
+	}
+
+	public void updateBidList(UpdateBidListDto bidList) {
+		BidList bidListToUpdate = mapper.updateBidListDtoToBidList(bidList);
+		repository.save(bidListToUpdate);
+	}
+
+	public void deleteBidList(Integer id) {
+		repository.deleteById(id);		
+	}
+
+}
