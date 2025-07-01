7d34333b22c0c8a0306f97bf7d7e6eb5c8b35c33
divineion <najima.guermoudi@gmail.com>
2025-07-02 00:48:16 +0200

    feat(exception): add CurvePointNotFoundException

---
+++ trading-app-demo/src/main/java/com/pcs/tradingapp/exceptions/CurvePointNotFoundException.java
@@ -0,0 +1,13 @@
+package com.pcs.tradingapp.exceptions;
+
+public class CurvePointNotFoundException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public CurvePointNotFoundException(String message) {
+		super(message);
+	}
+
+}
