2aabe07889245bdae2add0f866b615e43cfde62f
divineion <najima.guermoudi@gmail.com>
2025-07-02 00:51:02 +0200

    feat(endpoint): display CurvePoint update form
    Redirect to list with "errorMsg" attribute if CurvePoint is not found.

--- trading-app-demo/src/main/java/com/pcs/tradingapp/controllers/CurvePointController.java
+++ trading-app-demo/src/main/java/com/pcs/tradingapp/controllers/CurvePointController.java
@@ -11,10 +11,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.pcs.tradingapp.domain.CurvePoint;
 import com.pcs.tradingapp.dto.request.curvepoint.CreateCurvePointDto;
 import com.pcs.tradingapp.dto.response.CurvePointInfoDto;
+import com.pcs.tradingapp.exceptions.CurvePointNotFoundException;
 import com.pcs.tradingapp.services.curvepoint.CurvePointService;
 
 @Controller
@@ -49,9 +51,15 @@ public class CurvePointController {
     }
 
     @GetMapping("/curvepoint/update/{id}")
-    public String showUpdateForm(@PathVariable Integer id, Model model) {
-        // TODO: get CurvePoint by Id and to model then show to the form
-        return "curvePoint/update";
+    public String showUpdateForm(@PathVariable Integer id, Model model, RedirectAttributes redirectAttributes) {
+    	try {
+			CurvePointInfoDto curvePoint = service.getCurvePoint(id);
+			model.addAttribute("curvePoint", curvePoint);
+			return "curvePoint/update";
+		} catch (CurvePointNotFoundException e) {
+			redirectAttributes.addFlashAttribute("errorMsg", e.getMessage());
+			return "redirect:/curvepoint/list";
+		}
     }
 
     @PostMapping("/curvepoint/update/{id}")
