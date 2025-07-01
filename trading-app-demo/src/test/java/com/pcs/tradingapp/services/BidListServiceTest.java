2ad27c910b3eedc7a0b525aa92846cbe1972c5be
divineion <najima.guermoudi@gmail.com>
2025-07-01 15:11:11 +0200

    test(service): add unit test for fetch bid list throwing exception
    - mock repository to return empty Optional for unknown id
    - verify that BidListNotFoundException is thrown when service fetches
    unknown bid list

---
+++ trading-app-demo/src/test/java/com/pcs/tradingapp/services/BidListServiceTest.java
@@ -0,0 +1,33 @@
+package com.pcs.tradingapp.services;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.pcs.tradingapp.exceptions.BidListNotFoundException;
+import com.pcs.tradingapp.repositories.BidListRepository;
+
+@ExtendWith(MockitoExtension.class)
+public class BidListServiceTest {
+	@Mock
+	private BidListRepository repository;
+	
+	@InjectMocks
+	private BidListService service;
+	
+	@Test
+	void testFetchBidList_withUnknownId_shouldThrowException() {
+	    when(repository.findById(999)).thenReturn(Optional.empty());
+
+	    assertThrows(BidListNotFoundException.class, () -> {
+	        service.fetchUpdateBidListDtoById(999);
+	    });
+	}
+}
