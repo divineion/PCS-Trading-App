f46ce3c94df51160496cf9d53ae3a34e7d3157a2
divineion <najima.guermoudi@gmail.com>
2025-07-02 00:47:38 +0200

    feat(dto): add `UpdateCurvePointDto`
    Extends `CreateCurvePointDto`, with `id` field.

---
+++ trading-app-demo/src/main/java/com/pcs/tradingapp/dto/request/curvepoint/UpdateCurvePointDto.java
@@ -0,0 +1,13 @@
+package com.pcs.tradingapp.dto.request.curvepoint;
+
+public class UpdateCurvePointDto extends CreateCurvePointDto {
+	private int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+}
