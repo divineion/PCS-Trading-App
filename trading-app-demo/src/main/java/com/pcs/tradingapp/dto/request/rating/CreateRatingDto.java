3aedb7992167e3400b9cbb08771c0fa1372bf74e
divineion <najima.guermoudi@gmail.com>
2025-07-03 16:08:41 +0200

    feat(validation): add regex pattern validation to rating fields in DTO
    Annotate moodysRating, sandPRating, and fitchRating fields with @Pattern
    constraints to enforce valid rating formats.

--- trading-app-demo/src/main/java/com/pcs/tradingapp/dto/request/rating/CreateRatingDto.java
+++ trading-app-demo/src/main/java/com/pcs/tradingapp/dto/request/rating/CreateRatingDto.java
@@ -1,12 +1,19 @@
 package com.pcs.tradingapp.dto.request.rating;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 public class CreateRatingDto {
+	@Pattern(regexp = "^(Aaa|Aa1|Aa2|Aa3|A1|A2|A3|Baa1|Baa2|Baa3|Ba1|Ba2|Ba3|B1|B2|B3|Caa1|Caa2|Caa3|Ca|C)$",
+	         message = "Please enter a valid Moody's rating.")
 	private String moodysRating;
 	
+	@Pattern(regexp = "^(AAA|AA\\+|AA|AA\\-|A\\+|A|A\\-|BBB\\+|BBB|BBB\\-|BB\\+|BB|BB\\-|B\\+|B|B\\-|CCC\\+|CCC|CCC\\-|D)$",
+	         message = "Please enter a valid S&P rating.")
 	private String sandPRating;
 	
+	@Pattern(regexp = "^(AAA|AA\\+|AA|AA\\-|A\\+|A|A\\-|BBB\\+|BBB|BBB\\-|BB\\+|BB|BB\\-|B\\+|B|B\\-|CCC\\+|CCC|CCC\\-|D)$",
+	         message = "Please enter a valid Fitch rating.")
 	private String fitchRating;
 	
 	@NotNull
