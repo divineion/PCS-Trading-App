ed824f97d009694d78ee70ca96a2ab7fecae5751
divineion <najima.guermoudi@gmail.com>
2025-07-03 17:57:19 +0200

    feat(validation): use message variables for rating format validation
    In CreateRatingDto, replace hardcoded validation messages with message
    keys for Moody's, S&P, and Fitch rating fields.
    As order input is now of type text, entering a non-numeric value
    triggers a TypeMismatchException.

--- trading-app-demo/src/main/java/com/pcs/tradingapp/dto/request/rating/CreateRatingDto.java
+++ trading-app-demo/src/main/java/com/pcs/tradingapp/dto/request/rating/CreateRatingDto.java
@@ -1,22 +1,24 @@
 package com.pcs.tradingapp.dto.request.rating;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
 public class CreateRatingDto {
 	@Pattern(regexp = "^(Aaa|Aa1|Aa2|Aa3|A1|A2|A3|Baa1|Baa2|Baa3|Ba1|Ba2|Ba3|B1|B2|B3|Caa1|Caa2|Caa3|Ca|C)$",
-	         message = "Please enter a valid Moody's rating.")
+	         message = "{moodys.rating.format}")
 	private String moodysRating;
 	
 	@Pattern(regexp = "^(AAA|AA\\+|AA|AA\\-|A\\+|A|A\\-|BBB\\+|BBB|BBB\\-|BB\\+|BB|BB\\-|B\\+|B|B\\-|CCC\\+|CCC|CCC\\-|D)$",
-	         message = "Please enter a valid S&P rating.")
+	         message = "{sandp.rating.format}")
 	private String sandPRating;
 	
 	@Pattern(regexp = "^(AAA|AA\\+|AA|AA\\-|A\\+|A|A\\-|BBB\\+|BBB|BBB\\-|BB\\+|BB|BB\\-|B\\+|B|B\\-|CCC\\+|CCC|CCC\\-|D)$",
-	         message = "Please enter a valid Fitch rating.")
+	         message = "{fitch.rating.format}")
 	private String fitchRating;
 	
-	@NotNull
+	@Min(value = 1)
+	@NotNull(message = "{rating.order.digits}")
 	private Integer order;
 
 	public String getMoodysRating() {
