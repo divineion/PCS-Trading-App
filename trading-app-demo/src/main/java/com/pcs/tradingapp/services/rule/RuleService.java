a5c0b649c6a4fc06ad29c323afca2872843658b0
divineion <najima.guermoudi@gmail.com>
2025-07-04 01:04:44 +0200

    feat(service): add createRule() method
     - throw RuleNameAlreadyExistsException if name already
    exists,
     - map DTO to Rule entity via RuleMapper,
     - persist new rule via RuleRepository

--- trading-app-demo/src/main/java/com/pcs/tradingapp/services/rule/RuleService.java
+++ trading-app-demo/src/main/java/com/pcs/tradingapp/services/rule/RuleService.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Service;
 
 import com.pcs.tradingapp.constants.ApiMessages;
 import com.pcs.tradingapp.domain.Rule;
+import com.pcs.tradingapp.dto.request.rule.CreateRuleDto;
 import com.pcs.tradingapp.dto.response.RuleInfoDto;
+import com.pcs.tradingapp.exceptions.RuleNameAlreadyExistsException;
 import com.pcs.tradingapp.exceptions.RuleNotFoundException;
 import com.pcs.tradingapp.repositories.RuleRepository;
 
@@ -31,6 +33,16 @@ public class RuleService {
 				.orElseThrow(() -> new RuleNotFoundException(ApiMessages.RULE_NOT_FOUND));
 		
 		return mapper.ruleToRuleInfoDto(rule);
+	}
+
+	public void createRule(CreateRuleDto ruleDto) throws RuleNameAlreadyExistsException {
+		if (repository.findByName(ruleDto.getName()).isPresent()) {
+			throw new RuleNameAlreadyExistsException(ApiMessages.RULE_NAME_ALREADY_EXISTS);
+		}
+		
+		Rule rule = mapper.createRatingDtoToRating(ruleDto);
+		repository.save(rule);
+		
 	}
 
 }
