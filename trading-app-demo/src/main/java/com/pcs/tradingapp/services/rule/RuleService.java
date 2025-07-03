d5668909979e7e8eb16e6a352642726d1d73d03c
divineion <najima.guermoudi@gmail.com>
2025-07-04 00:24:22 +0200

    feat(service): add RuleService with fetch methods
     - add RuleService annotated with `@Service`,
     - implement getAllRules(): returns List<RuleInfoDto>, fetches from
    RuleRepository and maps entities to DTOs via RuleMapper,
     - implement getRuleById(): returns RuleInfoDto, fetches by id,
    throws RuleNotFoundException if not found, otherwise return DTO.

---
+++ trading-app-demo/src/main/java/com/pcs/tradingapp/services/rule/RuleService.java
@@ -0,0 +1,36 @@
+package com.pcs.tradingapp.services.rule;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.pcs.tradingapp.constants.ApiMessages;
+import com.pcs.tradingapp.domain.Rule;
+import com.pcs.tradingapp.dto.response.RuleInfoDto;
+import com.pcs.tradingapp.exceptions.RuleNotFoundException;
+import com.pcs.tradingapp.repositories.RuleRepository;
+
+@Service
+public class RuleService {
+	
+	private final RuleMapper mapper;
+	private RuleRepository repository;
+	
+	public RuleService(RuleMapper mapper, RuleRepository repository) {
+		this.mapper = mapper;
+		this.repository = repository;
+	}
+
+	public List<RuleInfoDto> getAllRules() {
+		List<Rule> rules = repository.findAll();
+		return mapper.rulesToRuleInfoDtos(rules);
+	}
+	
+	public RuleInfoDto getRuleById(int id) throws RuleNotFoundException {
+		Rule rule = repository.findById(id)
+				.orElseThrow(() -> new RuleNotFoundException(ApiMessages.RULE_NOT_FOUND));
+		
+		return mapper.ruleToRuleInfoDto(rule);
+	}
+
+}
