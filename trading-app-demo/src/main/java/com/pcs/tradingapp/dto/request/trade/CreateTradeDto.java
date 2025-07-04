1554708a1fac50feb75f31b319bd58e24ab28a99
divineion <najima.guermoudi@gmail.com>
2025-07-04 05:10:28 +0200

    feat(dto): add CreateTradeDto
    Add DTO containing all properties from Trade entity, excepted `id`

---
+++ trading-app-demo/src/main/java/com/pcs/tradingapp/dto/request/trade/CreateTradeDto.java
@@ -0,0 +1,42 @@
+package com.pcs.tradingapp.dto.request.trade;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Size;
+
+public class CreateTradeDto {
+	@Size(max=30)
+	@Column(nullable=false)
+	private String account;
+	
+	@Size(max=30)
+	@Column(nullable=false)
+	private String type;
+	
+	private BigDecimal buyQuantity;
+	
+	public String getAccount() {
+		return account;
+	}
+	
+	public void setAccount(String account) {
+		this.account = account;
+	}
+	
+	public String getType() {
+		return type;
+	}
+	
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	public BigDecimal getBuyQuantity() {
+		return buyQuantity;
+	}
+	
+	public void setBuyQuantity(BigDecimal buyQuantity) {
+		this.buyQuantity = buyQuantity;
+	}
+}
