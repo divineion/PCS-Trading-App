9f3b95bc8335483d173d9aa730207d15c0dd6a51
divineion <najima.guermoudi@gmail.com>
2025-07-04 05:12:52 +0200

    feat(mapper): add method to map DTO -> Rule entity
     - add `createTradeDtoToTrade(CreateTradeDto)`

--- trading-app-demo/src/main/java/com/pcs/tradingapp/servicestrade/TradeMapper.java
+++ trading-app-demo/src/main/java/com/pcs/tradingapp/servicestrade/TradeMapper.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.mapstruct.Mapper;
 
 import com.pcs.tradingapp.domain.Trade;
+import com.pcs.tradingapp.dto.request.trade.CreateTradeDto;
 import com.pcs.tradingapp.dto.response.TradeInfoDto;
 
 @Mapper(componentModel = "spring")
@@ -12,4 +13,6 @@ public interface TradeMapper {
 	public TradeInfoDto tradeToTradeInfoDto(Trade trade);
 	
 	public List<TradeInfoDto> tradesToTradeInfoDtos(List<Trade> trade);
+
+	public Trade createTradeDtoToTrade(CreateTradeDto tradeDto);
 }
