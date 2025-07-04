636988aa583d823a45fa95e09fe6fca4c551d26d
divineion <najima.guermoudi@gmail.com>
2025-07-04 05:38:48 +0200

    feat(mapper): add `updateTradeDtoToTrade()` method

--- trading-app-demo/src/main/java/com/pcs/tradingapp/servicestrade/TradeMapper.java
+++ trading-app-demo/src/main/java/com/pcs/tradingapp/servicestrade/TradeMapper.java
@@ -3,16 +3,20 @@ package com.pcs.tradingapp.servicestrade;
 import java.util.List;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 
 import com.pcs.tradingapp.domain.Trade;
 import com.pcs.tradingapp.dto.request.trade.CreateTradeDto;
+import com.pcs.tradingapp.dto.request.trade.UpdateTradeDto;
 import com.pcs.tradingapp.dto.response.TradeInfoDto;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface TradeMapper {
 	public TradeInfoDto tradeToTradeInfoDto(Trade trade);
 	
 	public List<TradeInfoDto> tradesToTradeInfoDtos(List<Trade> trade);
 
 	public Trade createTradeDtoToTrade(CreateTradeDto tradeDto);
+
+	public Trade updateTradeDtoToTrade(UpdateTradeDto tradeDto);
 }
