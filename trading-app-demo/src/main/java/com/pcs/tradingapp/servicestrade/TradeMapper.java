8ea6c7464ce596372895dc084f38ab08cdfd0d09
divineion <najima.guermoudi@gmail.com>
2025-07-04 04:45:36 +0200

    feat(mapper): add TradeMapper to map dto <-> entity
     - annotate class TradeMapper with MapStruct `@Mapper` annotation.
     - add 2 methods to map :
      - single Trade to TradeInfoDto;
      - list of entities to list of DTOs.

---
+++ trading-app-demo/src/main/java/com/pcs/tradingapp/servicestrade/TradeMapper.java
@@ -0,0 +1,15 @@
+package com.pcs.tradingapp.servicestrade;
+
+import java.util.List;
+
+import org.mapstruct.Mapper;
+
+import com.pcs.tradingapp.domain.Trade;
+import com.pcs.tradingapp.dto.response.TradeInfoDto;
+
+@Mapper(componentModel = "spring")
+public interface TradeMapper {
+	public TradeInfoDto tradeToTradeInfoDto(Trade trade);
+	
+	public List<TradeInfoDto> tradesToTradeInfoDtos(List<Trade> trade);
+}
