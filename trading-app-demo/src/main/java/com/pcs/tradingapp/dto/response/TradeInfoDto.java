11886e140b1766a71aeefd362e29fda14fccc046
divineion <najima.guermoudi@gmail.com>
2025-07-04 05:09:54 +0200

    fix(dto): add `id` property in record TradeInfoDto

--- trading-app-demo/src/main/java/com/pcs/tradingapp/dto/response/TradeInfoDto.java
+++ trading-app-demo/src/main/java/com/pcs/tradingapp/dto/response/TradeInfoDto.java
@@ -2,6 +2,6 @@ package com.pcs.tradingapp.dto.response;
 
 import java.math.BigDecimal;
 
-public record TradeInfoDto( String account, String type, BigDecimal buyQuantity) {
+public record TradeInfoDto(int id, String account, String type, BigDecimal buyQuantity) {
 
 }
