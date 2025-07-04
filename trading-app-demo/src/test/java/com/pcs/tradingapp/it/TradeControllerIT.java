c143c4f561b46452dc644e71916d44b3aa30d540
divineion <najima.guermoudi@gmail.com>
2025-07-04 15:41:31 +0200

    tests(controller): init IT class and test read and add endpoints
     - Create TradeControllerIT class annotated with:
      - `@DirtiesContext` to reset the context after each class,
      - `@Import` to temporarily import a minimal DevSecurityConfiguration
    class,
      - `@AutoConfigureWebMvc` to enable MVC test auto-configuration,
      - `@SpringBootTest` to provide the test context and enable the "test"
    profile.
    
     - Test endpoints:
      -GET `/trade/list`: returns list view with status 200 OK and a
    "trades" attribute,
      - GET `/trade/add`: returns add view with a "trade" attribute and
    status 200 OK,
      - POST `/trade/add` with valid data: redirects to "list" with status
    3xx,
      - POST `/trade/add` with blank required field: returns add view with
    an error on the "account" field and status 200 OK,
      - POST `/trade/add` with invalid data (string instead of decimal):
    returns add view with an error on the "buyQuantity" field and status 200
    OK.

---
+++ trading-app-demo/src/test/java/com/pcs/tradingapp/it/TradeControllerIT.java
@@ -0,0 +1,81 @@
+package com.pcs.tradingapp.it;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.pcs.tradingapp.config.DevSecurityConfiguration;
+
+@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
+@Import(DevSecurityConfiguration.class)
+@AutoConfigureMockMvc
+@SpringBootTest(properties = "spring.profiles.active=test")
+public class TradeControllerIT {
+		@Autowired
+		private MockMvc mockMvc;
+		
+		@Test
+		public void testIndex_shouldReturnListView() throws Exception {
+			mockMvc.perform(get("/trade/list"))
+				.andExpect(view().name("trade/list"))
+				.andExpect(model().attributeExists("trades"))
+				.andExpect(status().is2xxSuccessful());
+		}
+		
+		@Test
+	    public void testShowAddForm_shouldReturnAddFormView() throws Exception {
+	    	mockMvc.perform(get("/trade/add"))
+	    		.andExpect(view().name("trade/add"))
+	    		.andExpect(model().attributeExists("trade"))
+	    		.andExpect(status().is2xxSuccessful());
+	    }
+		
+		@Test
+	    public void testAddTrade_withValidData_shouldRedirectToList() throws Exception {
+	        mockMvc.perform(post("/trade/add")
+	                        .param("account", "AnyACC")
+	                        .param("type", "sell")
+	        				.param("buyQuantity", "10"))
+	                .andExpect(status().is3xxRedirection())
+	                .andDo(print())
+	                .andExpect(redirectedUrl("/trade/list"));
+		}
+		
+	    @Test
+	    public void testAddTrade_withBlankRequiredField_shouldReturnAddView() throws Exception {
+	        mockMvc.perform(post("/trade/add")
+	        		 .param("account", "")
+                     .param("type", "Sell")
+ 					 .param("buyQuantity", "10")	
+				)
+	            .andExpect(status().isOk())
+	            .andExpect(model().attributeHasFieldErrors("trade", "account"))
+	            .andExpect(view().name("trade/add"));
+	    }
+	    
+	    @Test
+	    public void testAddTrade_withInvalidData_shouldReturnAddView() throws Exception {
+	    	mockMvc.perform(post("/trade/add")
+	    			.param("account", "anyACC")
+	    			.param("type", "sell")
+	    			.param("buyQuantity", "five thousand")	
+	    			)
+	    	.andDo(print())
+	    	.andExpect(status().isOk())
+	    	.andExpect(model().attributeHasFieldErrors("trade", "buyQuantity"))
+	    	.andExpect(view().name("trade/add"));
+	    }
+}
