e63a0e0de306c697a631bef83995077cce6baf2e
divineion <najima.guermoudi@gmail.com>
2025-07-08 16:54:40 +0200

    tests(config): rename DevSecurityConfig to TestSecurityConfig
     - rename configuration to clarify test-only scope.
     - annotate configuration class with `@Profile("test")` to limit to test
    context.
     - update usage (in controller test classes, remove configuration
    imports).

--- trading-app-demo/src/test/java/com/pcs/tradingapp/it/CurvePointControllerIT.java
+++ trading-app-demo/src/test/java/com/pcs/tradingapp/it/CurvePointControllerIT.java
@@ -12,16 +12,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
-
-import com.pcs.tradingapp.config.DevSecurityConfiguration;
 
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @SpringBootTest(properties = "spring.profiles.active=test")
 @AutoConfigureMockMvc
-@Import(DevSecurityConfiguration.class)
 public class CurvePointControllerIT {
 	@Autowired
     private MockMvc mockMvc;
--- trading-app-demo/src/test/java/com/pcs/tradingapp/it/RatingControllerIT.java
+++ trading-app-demo/src/test/java/com/pcs/tradingapp/it/RatingControllerIT.java
@@ -12,14 +12,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 
-import com.pcs.tradingapp.config.DevSecurityConfiguration;
-
-@Import(DevSecurityConfiguration.class)
 @AutoConfigureMockMvc
 @SpringBootTest(properties = "spring.profiles.active=test")
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
