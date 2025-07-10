e6beb5cf4f51afe7516525fb825cd920accc1590
divineion <najima.guermoudi@gmail.com>
2025-07-10 15:58:41 +0200

    feat(security): add CustomUserDetails implementing UserDetails
    Add UserDetails implementation class to wrap the User entityand allow
    Spring Security to manage authentication details based on User entity.

---
+++ trading-app-demo/src/main/java/com/pcs/tradingapp/config/CustomUserDetails.java
@@ -0,0 +1,58 @@
+package com.pcs.tradingapp.config;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.pcs.tradingapp.domain.User;
+
+public class CustomUserDetails implements UserDetails {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private final User user;
+
+	public CustomUserDetails(User user) {
+		this.user = user;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return List.of(new SimpleGrantedAuthority(user.getRole().getName().name()));
+	}
+
+	@Override
+	public String getPassword() {
+		return user.getPassword();
+	}
+	
+	@Override
+	public String getUsername() {
+		return user.getUsername();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+}
