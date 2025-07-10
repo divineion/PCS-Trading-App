2eed406e8f6092cfba73314fadf1a301562e7fb2
divineion <najima.guermoudi@gmail.com>
2025-07-10 17:56:12 +0200

    fix(role): prefix GrantedAuthority with "ROLE_"
    Prefix each GrantedAuthority with "ROLE_" in CustomUserDetails to match
    the format expected by Spring Security.

--- trading-app-demo/src/main/java/com/pcs/tradingapp/config/CustomUserDetails.java
+++ trading-app-demo/src/main/java/com/pcs/tradingapp/config/CustomUserDetails.java
@@ -23,7 +23,7 @@ public class CustomUserDetails implements UserDetails {
 
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		return List.of(new SimpleGrantedAuthority(user.getRole().getName().name()));
+		return List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().getName().name()));
 	}
 
 	@Override
